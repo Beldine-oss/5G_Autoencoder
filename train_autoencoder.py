e2de2b569b15a580b315650563fc95361ef07739
SUMA <suma@example.com>
2026-03-19 14:32:47 +0300

    Update train_autoencoder and add requirements

---
+++ train_autoencoder.py
@@ -0,0 +1,9 @@
+# ===== IMPORT LIBRARIES =====
+
+import numpy as np
+import scipy.io as sio
+import matplotlib.pyplot as plt
+
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Input, Dense
+from tensorflow.keras.optimizers import Adam
